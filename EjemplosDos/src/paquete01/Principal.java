eecdf24e9481bb5de662b273649dcbf522a46a18
jhon pardo <jhonedupardo15@gmail.com>
2026-01-15 11:34:07 -0500

    EjemplosDos

--- EjemplosDos/src/paquete01/Principal.java
+++ EjemplosDos/src/paquete01/Principal.java
@@ -47,5 +47,5 @@ public class Principal {
 La problematica primero importa de los  demas paquetes para la resolucion del 
 problema mediante las funciones y cada funcion retorna un valor de dicha 
 funcion y esta presentando una solucion con el nombre, el apellido, ciudad, 
-y  al ingresar las 4 notas se calcula el promedio, 
+y  al ingresar las 4 notas va a calcular el promedio de las notas.
 */
